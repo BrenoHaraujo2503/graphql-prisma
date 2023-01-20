01cd855c01d72c8ffc52b907c3c4795b70686554
Breno Henrique <brenoharaujomorais@gmail.com>
2023-01-20 16:20:56 -0300

    find user by id query

--- src/resolvers/user-resolver.ts
+++ src/resolvers/user-resolver.ts
@@ -1,11 +1,21 @@
 import { Resolver, Query, Mutation, Arg, FieldResolver, Root } from 'type-graphql'
-import { CreateUserInput } from '../dtos/inputs/create-user-input';
+import { CreateUserInput } from '../dtos/inputs/users/create-user-input';
+import { GetUserInput } from '../dtos/inputs/users/get-user-input';
 import { Post } from '../dtos/models/post-model';
 import { User } from '../dtos/models/user-model';
 import { prisma } from '../libs/prisma';
 
 @Resolver(() => User)
 export class UserResolver {
+  @Query(() => User)
+  async findById(@Arg('data') data: GetUserInput) {
+    return prisma.user.findUnique({
+      where: {
+        id: data.id
+      }
+    })
+  }
+
   @Query(() => [User])
   async users() {
     return await prisma.user.findMany()
