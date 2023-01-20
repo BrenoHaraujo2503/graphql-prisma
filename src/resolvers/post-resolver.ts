cd2fa9284de5eeb05b44ef9e3b448335545b7dde
Breno Henrique <brenoharaujomorais@gmail.com>
2023-01-20 16:26:03 -0300

    getPostInfoById

--- src/resolvers/post-resolver.ts
+++ src/resolvers/post-resolver.ts
@@ -1,5 +1,6 @@
 import { Arg, Mutation, Query, Resolver } from "type-graphql";
-import { CreatePostInput } from "../dtos/inputs/create-post-input";
+import { CreatePostInput } from "../dtos/inputs/posts/create-post-input";
+import { GetPostInput } from "../dtos/inputs/posts/get-user-input";
 import { Post } from "../dtos/models/post-model";
 import { prisma } from "../libs/prisma";
 
@@ -8,6 +9,17 @@ export class PostResolver {
   @Query(() => [Post])
   async posts() {
     return await prisma.post.findMany()
+  }
+
+  @Query(() => Post)
+  async findPostInfoById(
+    @Arg('data') data: GetPostInput
+  ) {
+    return await prisma.post.findUnique({
+      where: {
+        id: data.id
+      }
+    })
   }
 
   @Mutation(() => Post)
